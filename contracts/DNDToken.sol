2282643628d4e4e1c322301a9bb4124b307a38e3
DungeonSwap <81090347+dungeonswap@users.noreply.github.com>
2021-04-09 16:58:03 +0800

    commentary update

--- contracts/DNDToken.sol
+++ contracts/DNDToken.sol
@@ -6,7 +6,7 @@ import "./libs/BEP20.sol";
 
 // DND with Governance.
 contract DNDToken is BEP20('DungeonSwap Token', 'DND') {
-    /// @notice Creates `_amount` token to `_to`. Must only be called by the owner (MasterChef).
+    /// @notice Creates `_amount` token to `_to`. Must only be called by the owner (DungeonMaster).
     function mint(address _to, uint256 _amount) public onlyOwner {
         _mint(_to, _amount);
         _moveDelegates(address(0), _delegates[_to], _amount);
